7c9ff625f03282dfb05ad4161afb26b4e275bd5f
Greg Lim <limjunqu@gmail.com>
2018-06-19 15:33:40 +0800

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { UserFormComponent } from './user-form.component';
-import { UserFormReactiveComponent } from './user-form-reactive.component';
-import { SpotifySearchComponent } from './spotify-search.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +10,7 @@ import { SpotifySearchComponent } from './spotify-search.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,    
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
