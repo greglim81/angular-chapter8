2597a6b4932f11901284cb9f1a3b6d094758a86e
Greg Lim <limjunqu@gmail.com>
2018-06-19 15:33:05 +0800

    Update app.component.ts
    updated for Angular 6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,22 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import { FormControl } from '@angular/forms'; 
-
+import { FormControl } from '@angular/forms';
+import { filter,debounceTime,distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',  
+  selector: 'app-root',
   template: `
-        <input class="form-control" type="search" 
- [formControl]="searchControl">                                               
-    `
+    <input class="form-control" type="search" 
+[formControl]="searchControl">      
+    `   
 })
 export class AppComponent {
   searchControl = new FormControl();
 
-
   constructor(){
-      this.searchControl.valueChanges
-          .filter(text => text.length >= 3)   
-          .debounceTime(400)   
-          .distinctUntilChanged()    
-          .subscribe(value => {
-            console.log(value);
-      });        
-  }
-}
+    this.searchControl.valueChanges
+      .pipe(filter(text => text.length >= 3), debounceTime(400),distinctUntilChanged())                
+      .subscribe(value => {
+        console.log(value);
+    });
+  }  
+}      
